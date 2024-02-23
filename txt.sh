b6f9c1d0a5a1c32a31d7fdd2adcf144d95e1899b
Akash <mailmeakashhalder@gmail.com>
2024-02-23 13:07:44 +0530

    test

--- txt.sh
+++ txt.sh
@@ -95,8 +95,7 @@ fi
 # Generate the genesis. This will generate validators based
 # on https://github.com/ethereum/eth2.0-pm/blob/a085c9870f3956d6228ed2a40cd37f0c6580ecd7/interop/mocked_start/README.md
 $PRYSM_CTL_BINARY testnet generate-genesis \
---fork=deneb \
---num-validators=$NUM_NODES \
+--num-validators=64 \
 --chain-config-file=./config.yml \
 --geth-genesis-json-in=./genesis.json \
 --output-ssz=$NETWORK_DIR/genesis.ssz \
