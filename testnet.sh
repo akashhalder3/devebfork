32cfccea3df8d2982a34759bb8522d649308b645
Akash <mailmeakashhalder@gmail.com>
2024-02-26 13:49:48 +0530

    test

--- testnet.sh
+++ testnet.sh
@@ -96,7 +96,7 @@ fi
 # on https://github.com/ethereum/eth2.0-pm/blob/a085c9870f3956d6228ed2a40cd37f0c6580ecd7/interop/mocked_start/README.md
 $PRYSM_CTL_BINARY testnet generate-genesis \
 --fork=capella \
---num-validators=3 \
+--num-validators=1 \
 --chain-config-file=./config.yml \
 --geth-genesis-json-in=./genesis.json \
 --output-ssz=$NETWORK_DIR/genesis.ssz \
