794bc97063877ee8d7d8a991b2b8536fd7d77d91
Akash <mailmeakashhalder@gmail.com>
2024-03-01 16:28:54 +0530

    test

--- testnet.sh
+++ testnet.sh
@@ -94,17 +94,17 @@ fi
 
 # Generate the genesis. This will generate validators based
 # on https://github.com/ethereum/eth2.0-pm/blob/a085c9870f3956d6228ed2a40cd37f0c6580ecd7/interop/mocked_start/README.md
-$PRYSM_CTL_BINARY testnet generate-genesis \
---fork=capella \
---num-validators=64 \
---chain-config-file=./config.yml \
---geth-genesis-json-in=./genesis.json \
---output-ssz=$NETWORK_DIR/genesis.ssz \
---geth-genesis-json-out=$NETWORK_DIR/genesis.json
+# $PRYSM_CTL_BINARY testnet generate-genesis \
+# --fork=capella \
+# --num-validators=64 \
+# --chain-config-file=./config.yml \
+# --geth-genesis-json-in=./genesis.json \
+# --output-ssz=$NETWORK_DIR/genesis.ssz \
+# --geth-genesis-json-out=$NETWORK_DIR/genesis.json
 
-# ./eth2-testnet-genesis capella --config=./config.yml --mnemonics=./mnemonics.yaml --eth1-config=./genesis.json
-# cp ./genesis.ssz  $NETWORK_DIR/
-# cp ./genesis.json  $NETWORK_DIR/
+./eth2-testnet-genesis capella --config=./config.yml --mnemonics=./mnemonics.yaml --eth1-config=./genesis.json
+cp ./genesis.ssz  $NETWORK_DIR/
+cp ./genesis.json  $NETWORK_DIR/
 
 # The prysm bootstrap node is set after the first loop, as the first
 # node is the bootstrap node. This is used for consensus client discovery
