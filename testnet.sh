15c37b09bf2a25292b0fff3731892519548915f7
Akash <mailmeakashhalder@gmail.com>
2024-03-01 16:39:33 +0530

    test

--- testnet.sh
+++ testnet.sh
@@ -94,13 +94,14 @@ fi
 
 # Generate the genesis. This will generate validators based
 # on https://github.com/ethereum/eth2.0-pm/blob/a085c9870f3956d6228ed2a40cd37f0c6580ecd7/interop/mocked_start/README.md
-# $PRYSM_CTL_BINARY testnet generate-genesis \
-# --fork=capella \
-# --num-validators=64 \
-# --chain-config-file=./config.yml \
-# --geth-genesis-json-in=./genesis.json \
-# --output-ssz=$NETWORK_DIR/genesis.ssz \
-# --geth-genesis-json-out=$NETWORK_DIR/genesis.json
+$PRYSM_CTL_BINARY testnet generate-genesis \
+--fork=capella \
+--num-validators=64 \
+--chain-config-file=./config.yml \
+--genesis-time-delay=600 \
+--geth-genesis-json-in=./genesis.json \
+--output-ssz=$NETWORK_DIR/genesis.ssz \
+--geth-genesis-json-out=$NETWORK_DIR/genesis.json
 
 ./eth2-testnet-genesis capella --config=./config.yml --mnemonics=./mnemonics.yaml --eth1-config=./genesis.json
 cp ./genesis.ssz  $NETWORK_DIR/
