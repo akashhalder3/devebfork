043e5b21c4d65e7a952ea74d5554857d78d9a338
Akash <mailmeakashhalder@gmail.com>
2024-03-13 14:38:30 +0530

    data

--- scripts/prepare-cl.sh
+++ scripts/prepare-cl.sh
@@ -64,7 +64,7 @@ echo "Generated $CONSENSUS_DIR/genesis.ssz"
 
 lcli \
 	generate-bootnode-enr \
-	--ip 0.0.0.0 \
+	--ip 20.40.53.142 \
 	--udp-port $CL_BOOTNODE_PORT \
 	--tcp-port $CL_BOOTNODE_PORT \
 	--genesis-fork-version $GENESIS_FORK_VERSION \
