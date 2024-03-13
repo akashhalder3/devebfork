09fa94923d5d40c71a445e82171a080248a6d521
Akash <mailmeakashhalder@gmail.com>
2024-03-13 11:57:37 +0530

    data

--- scripts/cl-bn-node.sh
+++ scripts/cl-bn-node.sh
@@ -38,7 +38,7 @@ $LIGHTHOUSE_CMD beacon_node \
 	--enable-private-discovery \
 	--staking \
 	--enr-address 127.0.0.1 \
-    --enr-address 20.40.53.142 \
+    --enr-address 0.0.0.0 \
 	--enr-udp-port $port \
 	--enr-tcp-port $port \
 	--port $port \
