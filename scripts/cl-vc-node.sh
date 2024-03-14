2d2b54f29726947a4b04c306ccb5d7d8cad7db60
Akash <mailmeakashhalder@gmail.com>
2024-03-14 12:13:59 +0530

    test

--- scripts/cl-vc-node.sh
+++ scripts/cl-vc-node.sh
@@ -25,6 +25,9 @@ $LIGHTHOUSE_CMD validator_client \
 	--init-slashing-protection \
     --beacon-nodes http://0.0.0.0:$(expr $BASE_CL_HTTP_PORT + $index) \
     --suggested-fee-recipient $(cat $SIGNER_EL_DATADIR/address) \
+    --http \
+    --http-address=0.0.0.0 \
+    --unencrypted-http-transport \
     < /dev/null > $log_file 2>&1
 
 if test $? -ne 0; then
