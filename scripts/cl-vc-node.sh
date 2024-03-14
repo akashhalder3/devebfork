9c844d2f9e3039289c3d01d2999edea5fe3f58f0
Akash <mailmeakashhalder@gmail.com>
2024-03-14 12:28:31 +0530

    test

--- scripts/cl-vc-node.sh
+++ scripts/cl-vc-node.sh
@@ -13,32 +13,17 @@ index=$1
 
 cl_data_dir $index
 datadir=$cl_data_dir
-
-http_port=$(expr $BASE_CL_HTTP_PORT + $index)
-# If index is 2, add 5 to the port
-if [[ $index -eq 2 ]]; then
-    port=$((BASE_CL_PORT + 5))
-else
-    port=$((BASE_CL_PORT + index))
-fi
-
-http_port=$((BASE_CL_HTTP_PORT + index))
 log_file=$datadir/validator_client.log
 
 echo "Started the lighthouse validator client #$index. You can see the log at $log_file"
 
 # Send all the fee to the PoA signer
 $LIGHTHOUSE_CMD validator_client \
-    --debug-level=info \
     --datadir $datadir \
 	--testnet-dir $CONSENSUS_DIR \
 	--init-slashing-protection \
     --beacon-nodes http://0.0.0.0:$(expr $BASE_CL_HTTP_PORT + $index) \
     --suggested-fee-recipient $(cat $SIGNER_EL_DATADIR/address) \
-    --http \
-    --http-address=0.0.0.0 \
-    --http-port=$port \
-    --unencrypted-http-transport \
     < /dev/null > $log_file 2>&1
 
 if test $? -ne 0; then
