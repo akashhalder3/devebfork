2947145fc099c115ad48babeb1e606239d816972
Akash <mailmeakashhalder@gmail.com>
2024-03-14 16:10:43 +0530

    test

--- scripts/el-node.sh
+++ scripts/el-node.sh
@@ -17,17 +17,29 @@ datadir=$el_data_dir
 address=$(cat $datadir/address)
 port=$(expr $BASE_EL_PORT + $index)
 rpc_port=$(expr $BASE_EL_RPC_PORT + $index)
+http_port=$(expr $BASE_EL_RPC_PORT + $index)
 log_file=$datadir/geth.log
 
 echo "Started the geth node #$index which is now listening at port $port and rpc at port $rpc_port. You can see the log at $log_file"
 $GETH_CMD \
     --datadir $datadir \
+    --authrpc.addr "0.0.0.0" \
     --authrpc.port $rpc_port \
+    --authrpc.vhosts "*" \
     --port $port \
     --bootnodes $boot_enode \
     --networkid $NETWORK_ID \
     --unlock $address \
     --password $ROOT/password \
+    --http  \
+    --http.addr "0.0.0.0" \
+    --http.port $http_port \
+    --http.corsdomain "*" \
+    --http.api admin,engine,net,eth,web3,debug \
+    --syncmode full \
+    --rpc.allow-unprotected-txs \
+    --allow-insecure-unlock
+    --nat=extip:20.40.53.142 \
     < /dev/null > $log_file 2>&1
 
 if test $? -ne 0; then
